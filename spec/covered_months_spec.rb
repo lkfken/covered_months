b61ae9714c2b4c19fb75ffa5f390d332d924c724
Kenneth Leung <kenneth@leungs.us>
2017-05-18 11:24:03 -0700

    refactoring

--- spec/covered_months_spec.rb
+++ spec/covered_months_spec.rb
@@ -8,11 +8,11 @@ describe CoveredMonths do
     let(:date_segments) { build(:active_member).dates }
     let(:covered_months) { CoveredMonths.create(date_segments: date_segments, base_date_segment: (start_date..end_date)) }
 
-    it '#range_dates' do
-      expect(covered_months.range_dates.size).to eq(366)
+    it '#dates' do
+      expect(covered_months.dates.size).to eq(366)
     end
 
-    it "#count" do
+    it '#count' do
       expect(covered_months.count).to eq(12.0)
     end
   end
@@ -21,11 +21,11 @@ describe CoveredMonths do
     let(:date_segments) { build(:mid_year_member).dates }
     let(:covered_months) { CoveredMonths.create(date_segments: date_segments, base_date_segment: (start_date..end_date)) }
 
-    it '#range_dates' do
-      expect(covered_months.range_dates.size).to eq(121)
+    it '#dates' do
+      expect(covered_months.dates.size).to eq(121)
     end
 
-    it "#count" do
+    it '#count' do
       expect(covered_months.count).to eq(4.0)
     end
   end
@@ -34,11 +34,11 @@ describe CoveredMonths do
     let(:date_segments) { build(:partial_month_member).dates }
     let(:covered_months) { CoveredMonths.create(date_segments: date_segments, base_date_segment: (start_date..end_date)) }
 
-    it '#range_dates' do
-      expect(covered_months.range_dates.size).to eq(15)
+    it '#dates' do
+      expect(covered_months.dates.size).to eq(15)
     end
 
-    it "#count" do
+    it '#count' do
       expect(covered_months.count).to eq(0.5)
     end
   end
@@ -47,12 +47,26 @@ describe CoveredMonths do
     let(:date_segments) { build(:multiple_segments_member).dates }
     let(:covered_months) { CoveredMonths.create(date_segments: date_segments, base_date_segment: (start_date..end_date)) }
 
-    it '#range_dates' do
-      expect(covered_months.range_dates.size).to eq(30)
+    it '#dates' do
+      expect(covered_months.dates.size).to eq(30)
     end
 
-    it "#count" do
+    it '#count' do
       expect(covered_months.count).to eq(1.0)
     end
   end
+
+  context 'over edge segments coverage' do
+    let(:date_segments) { build(:over_edge_segments_member).dates }
+    let(:covered_months) { CoveredMonths.create(date_segments: date_segments, base_date_segment: (start_date..end_date)) }
+
+    it '#dates' do
+      expect(covered_months.dates.size).to eq(123)
+    end
+
+    it '#count' do
+      expect(covered_months.count).to eq(4.0)
+    end
+  end
+
 end
